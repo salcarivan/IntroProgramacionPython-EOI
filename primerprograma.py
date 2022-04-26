7bb383109c3dc7ca9ebccb049037dc0f328151e0
salcarivan <ivancarrascal97@gmail.com>
2022-04-26 20:27:05 +0200

    primeros rogramas con python

---
+++ primerprograma.py
@@ -0,0 +1,20 @@
+#comentarios
+#este es un primer progrma en python
+
+texto1linea =" este testo se aasigna en una linea"
+
+textdistintaslineas= "Escribir valores alfanumericos \
+en distintas lineas"
+
+#declaraciones de variables
+numero =10
+Numero= 20
+saludo= "hola internet"
+print(numero)
+print(Numero)
+print (numero+Numero)
+print ("saludos"+ saludo)
+print(type(numero))
+print(type(saludo))
+#del numero
+print (numero)
---
+++ segudoprograma.py
@@ -0,0 +1,16 @@
+#asignación secundaria
+from re import A
+
+
+a=5
+b=10
+print("Paso 1. Valores iniciales")
+print(a)
+print(b)
+c=b
+b=a
+a=c
+del c
+print("Paso 2. Valores intercambiados")
+print (a)
+print (b)
