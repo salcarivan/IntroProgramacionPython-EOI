95bde1e3b007db5ef3a67b91712568af3f7f50a4
salcarivan <ivancarrascal97@gmail.com>
2022-04-27 09:57:25 +0200

    correciones001

---
+++ zonashorarias.py
@@ -0,0 +1,6 @@
+#zonas horarias
+from datetime import datetime
+from pytz import timezone
+import pytz
+print (datetime.now(pytz.timezone("Asia/Tokyo")))
+print (datetime.now(pytz.timezone("Europe/Madrid")))
