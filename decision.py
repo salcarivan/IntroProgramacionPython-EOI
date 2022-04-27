38e86c65515ae01c2058ab151994e359faefb1d9
salcarivan <ivancarrascal97@gmail.com>
2022-04-27 11:27:14 +0200

    correcciones 1

--- decision.py
+++ decision.py
@@ -1,9 +1,8 @@
 a = 20
 b = 20
 if (a>b):
-    print(f"el numero mayor es {a}")
+    print(f"1-el numero mayor es {a}")
+elif (b>a):
+        print(f"2-el numeto mayor es {b}")
 else:
-    if (b>a):
-        print(f"el numeto mayor es {b}")
-    else:
-        print("los dos numeros son iguales")
+        print("3-los dos numeros son iguales")
