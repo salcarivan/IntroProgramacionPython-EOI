36cb381429f6b18894239192bb7e112ad58cb86e
salcarivan <ivancarrascal97@gmail.com>
2022-04-27 13:33:13 +0200

    final de la mañana

---
+++ ejercontinue.py
@@ -0,0 +1,11 @@
+contadornumero=0
+for numero in range (1,21,2):
+    contadornumero+=1
+    if(contadornumero<5):
+        break #salta a la linea despues del bloque for
+    print(numero)
+    #print("otra instruccion 1")
+    #print("otra instruccion 2")
+    #print("otra instruccion 3")
+    #print ("otra instuccion 4")
+print("numeros de la serie", contadornumero)
