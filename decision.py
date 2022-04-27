d12dd6a2ec7fdb8801c2172a63951585f4a84adb
salcarivan <ivancarrascal97@gmail.com>
2022-04-27 11:26:08 +0200

    nuevos ejercicios

---
+++ decision.py
@@ -0,0 +1,9 @@
+a = 20
+b = 20
+if (a>b):
+    print(f"el numero mayor es {a}")
+else:
+    if (b>a):
+        print(f"el numeto mayor es {b}")
+    else:
+        print("los dos numeros son iguales")
